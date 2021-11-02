2aaa69d491fb215c01245e9fcb1cb96b74d9ed70
Jagannath R Kulakarni <jagannathrkulakarni.171845@gmail.com>
2021-11-02 14:20:03 +0530

    some changes to listener and publisher

--- app/nats-test/src/listener.ts
+++ app/nats-test/src/listener.ts
@@ -9,8 +9,19 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 
 stan.on('connect', () => {
   console.log('Listener connected to NATS');
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true); //If an event is received acknoledgment is created(by default if we loses db then no acknolegment is saved so we give this option)
+   
+    stan.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
 
-  const subsricbtion=stan.subscribe('ticket:created');
+  const subsricbtion=stan.subscribe('ticket:created',
+  'orders-service-queue-group',  //This is a queue-group and if two services are created of this type then NATS server sends event to any one of them
+  options
+  );
 
   subsricbtion.on('message',(msg : Message)=>{
     console.log('Message recieved');
@@ -19,21 +30,16 @@ stan.on('connect', () => {
     if(typeof data === 'string'){
       console.log(`Received Event #${msg.getSequence()}, with data: ${data}`)
     }
+
+    msg.ack();
   })
-  // stan.on('close', () => {
-  //   console.log('NATS connection closed!');
-  //   process.exit();
-  // });
 
   // const options = stan
   //   .subscriptionOptions()
   //   .setManualAckMode(true)
   //   .setDeliverAllAvailable()
   //   .setDurableName('accounting-service');
-
-  // const subscription = stan.subscribe(
-  //   'ticket:created',
-  //   'queue-group-name',
-  //   options
-  // );
 });
+
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
